cddece963be59ea7f0fb6a98ba292167b5914184
Jibrilos <webxite23@gmail.com>
2021-03-07 12:11:44 +0200

    i added a button component

---
+++ src/components/Buttons/button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./button.css";
+
+function Button() {
+  return (
+    <div className="btn">
+      <a href="/" className="btn-1 outer-shadow hover-in-shadow">
+        More About Me
+      </a>
+    </div>
+  );
+}
+
+export default Button;
